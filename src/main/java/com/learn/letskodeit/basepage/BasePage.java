efa1efdbb822edf7af0b46fc87a2bff5bcfeec22
PradipDhanani <pradip.dhanani@gmail.com>
2020-04-14 15:28:25 +0100

    lets kode it-maven

---
+++ src/main/java/com/learn/letskodeit/basepage/BasePage.java
@@ -0,0 +1,11 @@
+package com.learn.letskodeit.basepage;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Pradip
+ */
+public class BasePage {
+    //define web driver
+    public static WebDriver driver;
+}
